cb195dfb9fd6ae18034651ae70ef4a0cf86a11c5
Devansh Agrawal <61635654+oktopuslinky@users.noreply.github.com>
2025-06-19 17:12:17 -0500

    Time: 138 ms (23.10%) Space: 29.5 MB (91.82%)

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -13,10 +13,6 @@ class Solution(object):
         groupNum = 0
         while groupNum < len(nums)/3:
             to_append = nums[groupNum*3:groupNum*3+3]
-            print to_append
-            print to_append[1] - to_append[0]
-            print to_append[2] - to_append[1]
-            print to_append[2] - to_append[0]
             if to_append[1] - to_append[0] <= k and to_append[2] - to_append[1] <= k and to_append[2] - to_append[0] <= k:
                 output.append(to_append)
             else:
