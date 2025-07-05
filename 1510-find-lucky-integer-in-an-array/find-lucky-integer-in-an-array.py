677f5b148a9e3487e33cb06510bd7993664ca14f
Devansh Agrawal <61635654+oktopuslinky@users.noreply.github.com>
2025-07-05 12:34:33 -0500

    Time: 0 ms (100.00%) Space: 12.5 MB (84.59%)

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -4,14 +4,15 @@ class Solution(object):
         :type arr: List[int]
         :rtype: int
         """
+        arr.sort()
         freqs = {}
         for integer in arr:
             freqs[integer] = freqs.get(integer, 0) + 1
         
         largest_lucky = -1
-        for integer, freq in freqs.items():
-            print integer, freq
+        for integer, freq in reversed(list(freqs.items())):
             if integer == freq:
-                largest_lucky = max(largest_lucky, integer)
+                return integer
+                #largest_lucky = max(largest_lucky, integer)
         
         return largest_lucky
