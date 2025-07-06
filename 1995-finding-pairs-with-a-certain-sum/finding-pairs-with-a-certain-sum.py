bea62f739f6b720a2cf9cba12840d3f4266d6a52
Devansh Agrawal <61635654+oktopuslinky@users.noreply.github.com>
2025-07-06 12:29:47 -0500

    Time: 265 ms (91.18%) Space: 34.2 MB (23.53%)

---
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.py
@@ -0,0 +1,44 @@
+class FindSumPairs(object):
+
+    def __init__(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        """
+        self.nums1 = {}
+        for num in nums1:
+            self.nums1[num] = self.nums1.get(num, 0) + 1
+        self.nums2 = nums2
+        self.nums2Count = {}
+        for num in nums2:
+            self.nums2Count[num] = self.nums2Count.get(num, 0) + 1
+
+    def add(self, index, val):
+        """
+        :type index: int
+        :type val: int
+        :rtype: None
+        """
+        #print index, val, self.nums2
+        changeVal = self.nums2[index]
+        self.nums2Count[changeVal] -= 1
+        changeVal += val
+        self.nums2[index] = changeVal
+        self.nums2Count[changeVal] = self.nums2Count.get(changeVal, 0) + 1
+
+    def count(self, tot):
+        """
+        :type tot: int
+        :rtype: int
+        """
+        output = 0
+
+        for num, count in self.nums1.items():
+            to_find = tot-num
+            if to_find in self.nums2Count:
+                output += self.nums1[num] * self.nums2Count[to_find]
+        return output
+# Your FindSumPairs object will be instantiated and called as such:
+# obj = FindSumPairs(nums1, nums2)
+# obj.add(index,val)
+# param_2 = obj.count(tot)
