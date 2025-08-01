51d659ec27180934c007ca4e87ad14186dab232a
Devansh Agrawal <61635654+oktopuslinky@users.noreply.github.com>
2025-08-01 12:23:34 -0500

    Time: 0 ms (100.00%) Space: 12.5 MB (21.66%)

--- 0118-pascals-triangle/pascals-triangle.py
+++ 0118-pascals-triangle/pascals-triangle.py
@@ -8,14 +8,13 @@ class Solution(object):
         output = [[1], [1,1]]
         if numRows < 3:
             return output[:numRows]
+
         for rowNum in range(2, numRows):
             prevRow = output[rowNum-1]
             newRow = [1]
             for i in range(1, len(prevRow)):
-                #print i, prevRow
                 newRow.append(prevRow[i] + prevRow[i-1])
             newRow.append(1)
             output.append(newRow)
-            #print 'OUTPUT:', output
 
         return output
