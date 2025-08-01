7ae789e78d8fe641b56fb683e39a69f9bf4b4188
Devansh Agrawal <61635654+oktopuslinky@users.noreply.github.com>
2025-08-01 12:23:20 -0500

    Time: 0 ms (100.00%) Space: 12.6 MB (21.66%)

---
+++ 0118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        
+        output = [[1], [1,1]]
+        if numRows < 3:
+            return output[:numRows]
+        for rowNum in range(2, numRows):
+            prevRow = output[rowNum-1]
+            newRow = [1]
+            for i in range(1, len(prevRow)):
+                #print i, prevRow
+                newRow.append(prevRow[i] + prevRow[i-1])
+            newRow.append(1)
+            output.append(newRow)
+            #print 'OUTPUT:', output
+
+        return output
