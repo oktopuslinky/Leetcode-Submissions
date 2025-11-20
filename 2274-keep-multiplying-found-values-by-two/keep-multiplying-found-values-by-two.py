0a6396d334725d50894d201a13b1647c173a0de6
Devansh Agrawal <61635654+oktopuslinky@users.noreply.github.com>
2025-11-20 16:37:05 -0600

    Time: 1 ms (37.08%) Space: 12.5 MB (84.41%)

---
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        """
+        :type nums: List[int]
+        :type original: int
+        :rtype: int
+        """        
+        for num in nums:
+            if original in nums:
+                original *= 2
+            else:
+                return original
+        return original
