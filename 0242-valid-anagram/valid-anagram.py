f9e727cd676d6f3d393327e20f51316de834b4c0
Devansh Agrawal <61635654+oktopuslinky@users.noreply.github.com>
2025-12-23 13:23:51 -0600

    Time: 23 ms (52.44%) Space: 14.3 MB (18.28%)

--- 0242-valid-anagram/valid-anagram.py
+++ 0242-valid-anagram/valid-anagram.py
@@ -6,10 +6,11 @@ class Solution(object):
         :rtype: bool
         """
         s_letters, t_letters = {}, {}
-
+        if len(s) != len(t): return False
+        
         for i in range(len(s)):
             s_letters[s[i]] = s_letters.get(s[i], 0) + 1
-        for i in range(len(t)):
             t_letters[t[i]] = t_letters.get(t[i], 0) + 1
         
+        print s_letters, t_letters
         return s_letters == t_letters
