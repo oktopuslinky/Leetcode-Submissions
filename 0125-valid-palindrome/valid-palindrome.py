b6a2cc95ffb5b607bf5445931ecbb826b7c5b9d9
Devansh Agrawal <61635654+oktopuslinky@users.noreply.github.com>
2025-12-30 11:45:32 -0600

    Time: 14 ms (71.46%) Space: 12.6 MB (88.23%)

---
+++ 0125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.lower()
+        l, r = 0, len(s)-1
+
+        while l < r:
+            while not s[l].isalnum() and l < r:
+                l += 1
+            while not s[r].isalnum() and l < r:
+                r -= 1
+            #print l, r, s[l], s[r]
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+
+        return True
