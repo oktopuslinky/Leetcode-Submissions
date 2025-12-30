18c55a2626597b128ff4435725f4b709e1224f15
Devansh Agrawal <61635654+oktopuslinky@users.noreply.github.com>
2025-12-30 14:12:38 -0600

    Time: 2725 ms (13.13%) Space: 20.3 MB (7.66%)

---
+++ 0015-3sum/3sum.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        triplets = set()
+        nums.sort()
+
+        print nums
+
+        for i, num in enumerate(nums[:-2]):
+            target = -num
+            l, r = i+1, len(nums)-1
+            while l < r:
+                #print nums[i], nums[l], nums[r]
+                if nums[l] + nums[r] > target:
+                    #print(l, r, target, '>')
+                    r -= 1
+                if nums[l] + nums[r] < target:
+                    #print(l, r, target, '<')
+                    l += 1
+                if nums[l] + nums[r] == target and l != r:
+                    triplets.add((nums[i], nums[l], nums[r]))
+                    l += 1
+                    r -= 1
+        return [list(_) for _ in triplets]
