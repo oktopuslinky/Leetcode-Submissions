7ddc40b26c1f1e8513c8a93d8e24d483368e62d7
Devansh Agrawal <61635654+oktopuslinky@users.noreply.github.com>
2026-01-13 20:15:35 -0600

    Time: 553 ms (18.36%) Space: 0B (100.00%)

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT project_id, ROUND(AVG(experience_years),2) as average_years
+SELECT project_id, ROUND(AVG(experience_years), 2) as average_years
 FROM (Employee NATURAL JOIN Project as j)
-GROUP BY project_id
+GROUP BY project_id;
